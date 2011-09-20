9c61f09ee9af7f28a40b27d5782a0a23e4e71653
Evan Senter <evansenter@gmail.com>
2011-09-19 23:22:56 -0400

    Pushing so I can change gears.

--- nussinov.rb
+++ nussinov.rb
@@ -1,3 +1,4 @@
+require "set"
 require "awesome_print"
 require "../lagrange/lagrange.rb"
 
@@ -5,7 +6,7 @@ module Rnabor
   class Nussinov
     TEMPERATURE        = 37
     BOLTZMANN_CONSTANT = 0.0019872370936902486 # kcal / mol / K
-    BASE_PAIR_ENERGY   = Math::E ** (1 / (BOLTZMANN_CONSTANT * (TEMPERATURE + 273.15)))
+    BASE_PAIR_ENERGY   = 0 # Math::E ** (1 / (BOLTZMANN_CONSTANT * (TEMPERATURE + 273.15)))
     MIN_LOOP_SIZE      = 3
     BASE_PAIRINGS      = {
       "a" => %w[u],
@@ -44,12 +45,7 @@ module Rnabor
           
           j_unpaired_contribution = table_at(i, j - 1) * x_value ** (end_base_paired?(i, j) ? 1 : 0)
           j_paired_contribution   = (i..(j - MIN_LOOP_SIZE - 1)).select { |k| can_pair?(k, j) }.inject(0.0) do |sum, k|
-            i_j_pairs                  = count_pairs(match_pairs(structure[i..j]))
-            upstream_partition_pairs   = k - 1 < i ? 0 : pair_distance(structure[i..j], structure[i..(k - 1)])
-            downstream_partition_pairs = pair_distance(structure[i..j], structure[(k + 1)..(j - 1)])
-            base_pair_difference       = i_j_pairs - upstream_partition_pairs - downstream_partition_pairs + (paired?(k, j) ? -1 : 1)
-            
-            sum + BASE_PAIR_ENERGY * (k - 1 < i ? 1.0 : table_at(i, k - 1)) * table_at(k + 1, j - 1) * x_value ** base_pair_difference
+            sum + BASE_PAIR_ENERGY * (k - 1 < i ? 1.0 : table_at(i, k - 1)) * table_at(k + 1, j - 1) * x_value ** pair_distance(i, k, j)
           end
           
           table_at(i, j, j_unpaired_contribution + j_paired_contribution)
@@ -59,11 +55,13 @@ module Rnabor
       table_at(1, length)
     end
     
-    def pair_distance(structure_a, structure_b)
-      structure_a_pairings = closed_pairs(match_pairs(structure_a)).map(&:sort).to_set
-      structure_b_pairings = closed_pairs(match_pairs(structure_b)).map(&:sort).to_set
+    def pair_distance(i, k, j)
+      reference_structure  = closed_pairs(match_pairs(structure[i..j])).map { |from, to| [from + i - 1, to + i - 1] }.to_set
+      upstream             = closed_pairs(match_pairs(k - 1 < i ? "" : structure[i..(k - 1)])).map { |from, to| [from + i - 1, to + i - 1] }
+      downstream           = closed_pairs(match_pairs(structure[(k + 1)..(j - 1)])).map { |from, to| [from + k, to + k] }
+      comparitive_pairings = (upstream + downstream + [[k, j]]).to_set
       
-      ((structure_a_pairings - structure_b_pairings) + (structure_b_pairings - structure_a_pairings)).size
+      ((reference_structure - comparitive_pairings) + (comparitive_pairings - reference_structure)).size
     end
 
     def match_pairs(structure_to_match = structure)
@@ -130,7 +128,7 @@ module Rnabor
 end
 
 rna = Rnabor::Nussinov.new(
-  "gggccc", 
-  "......"
+  "ggggcccc", 
+  "(.(...))"
 )
 rna.partition_function
